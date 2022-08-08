c9de6591b0a8fb4fdb7ff54cf0edad4212786137
ITB\andrey.chikurin <andrej.chikurin@altezza.org>
2022-08-08 11:24:08 +0300

    added mapping

--- BuberDinner.Application/Authentication/Commands/Register/RegisterCommandHandler.cs
+++ BuberDinner.Application/Authentication/Commands/Register/RegisterCommandHandler.cs
@@ -22,6 +22,8 @@ public class RegisterCommandHandler :
 
     public async Task<ErrorOr<AuthenticationResult>> Handle(RegisterCommand command, CancellationToken cancellationToken)
     {
+        await Task.CompletedTask;
+
         // 1. Validate the use doesn't exist
         if (_userRepository.GetUserByEmail(command.Email) is not null)
         {
--- BuberDinner.Contracts/Authentication/AuthenticationResponse.cs
+++ BuberDinner.Contracts/Authentication/AuthenticationResponse.cs
@@ -2,7 +2,7 @@ namespace BuberDinner.Contracts.Authentication;
 
 public record AuthenticationResponse(
     Guid Id,
-    string Firstname,
+    string FirstName,
     string LastName,
     string Email,
     string Token);
